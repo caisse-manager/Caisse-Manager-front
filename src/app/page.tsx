30117a49047a1809149b635cd814a5d9b706118d
YekhlefAya <yekhleffaya@gmail.com>
2025-07-07 09:12:19 +0100

    FAQ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white">
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white max-x-screen">
       <NavBar />      
       <Contact />
       <PreviousWork />
--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -4,11 +4,37 @@ import { useState } from 'react';
 import { ArrowUpRight } from 'lucide-react';
 import Image from "next/image"
 import { FaWhatsapp } from 'react-icons/fa';
+import { motion, AnimatePresence } from 'framer-motion';
+
 
 export default function FAQ() {
   const [openIndex, setOpenIndex] = useState<number | null>(null);
 
   const faqs = [
+    {
+      question: "What are your focus areas as a UI/UX design agency?",
+      answer: "We focus on user-centered design, usability testing, and creating intuitive interfaces.",
+    },
+    {
+      question: "Why is UI/UX design important for your business growth?",
+      answer: "Good design improves user engagement and conversion rates, driving business growth.",
+    },
+    {
+      question: "My website isn't generating enough leads. How can your design help?",
+      answer: "We analyze user behavior to optimize the design for better lead generation.",
+    },
+    {
+      question: "What separates Wavespace from other top UI/UX design agencies?",
+      answer: "Our personalized approach and deep understanding of client needs set us apart.",
+    },
+    {
+      question: "How could you help us redesign our app, website, or enterprise/B2B software?",
+      answer: "We provide end-to-end design services, from user research to prototyping and testing.",
+    },
+    {
+      question: "Do you work with startups or only with B2B/enterprise companies?",
+      answer: "We work with a range of clients including startups, SMBs, and enterprises.",
+    },
     {
       question: "What are your focus areas as a UI/UX design agency?",
       answer: "We focus on user-centered design, usability testing, and creating intuitive interfaces.",
@@ -42,15 +68,15 @@ export default function FAQ() {
   return (
     
     <div className="p-4 container mx-auto min-h-screen ">
-      <h1 className="text-4xl lg:text-7xl font-light text-black dark:text-white mb-6 leading-tight ">
+      <h1 className="text-5xl lg:text-4xl font-bold text-black dark:text-white mb-6 leading-snug p-4">
         Frequently Asked <br /> Questions
-      </h1>
+    </h1>
       <div className="flex justify-between flex-wrap">
-      <ul className="max-w-6xl pt-4 mb-6 ">
+        <ul className="max-w-3xl w-full pt-4 mb-6">
         {faqs.map((faq, index) => (
           <li
             key={index}
-            className="border-b border-gray-300 p-4 cursor-pointer dark:border-gray-700"
+            className=" w-full border-b border-gray-300 p-4 cursor-pointer dark:border-gray-700"
             onClick={() => toggleFAQ(index)}
           >
             <div className="flex justify-between ">
@@ -62,19 +88,26 @@ export default function FAQ() {
               />
             </div>
 
-            {openIndex === index && (
-              <p
-                id={`faq-answer-${index}`}
-                className="mt-2 text-gray-700 dark:text-gray-300"
-              >
-                {faq.answer}
-              </p>
-            )}
+            <AnimatePresence>
+                {openIndex === index && (
+                    <motion.p
+                    id={`faq-answer-${index}`}
+                    className="mt-2 text-gray-700 dark:text-gray-300"
+                    initial={{ opacity: 0, height: 0 }}
+                    animate={{ opacity: 1, height: "auto" }}
+                    exit={{ opacity: 0, height: 0 }}
+                    transition={{ duration: 0.4, ease: "easeInOut" }}
+                    >
+                    {faq.answer}
+                    </motion.p>
+                )}
+            </AnimatePresence>
+
           </li>
         ))}
       </ul>
-      <section className="max-w-4xl mr-6 ">
-        <div className="border dark:border-gray-500 border-gray-700 rounded-md p-4" >
+      <section className="max-w-4xl mx-auto flex justify-center h-full sticky top-5 self-start">
+        <div className="border dark:border-gray-500 border-gray-700 rounded-md p-4 " >
             <Image src="/caisse-manager-logo.png"  alt="caisse manager logo " width={100} height={100} />
             <p className="mt-2 text-gray-700 dark:text-gray-300 max-w-xs ">
             Hi, I&apos;m Shahid, the CEO and Founder of Wavespace. Don&apos;t hesitate to reach out to me anytime - I&apos;m here to answer all your questions!
